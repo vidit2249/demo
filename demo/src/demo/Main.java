262f2e24bd196c80facc1d604b8087b56a7ca818
sdac-11 <sdac-11@sdac-11-PC>
2024-02-02 16:45:48 +0530

    try

--- demo/src/demo/Main.java
+++ demo/src/demo/Main.java
@@ -3,6 +3,6 @@ package demo;
 public class Main {
 	public static void main(String[] args) {
 		System.out.println("hello github");
-		System.out.println("mick testing 1234");
+		System.out.println("mick testing 1234"); 
 	}
 }
